744423a2e646adcd16d57cadaa1216e72cafcaa3
Matthew <769438@student.dusd.net>
2025-11-21 13:46:00 -0800

    Did some of the movement for the character and added decorations

---
+++ P6Davila2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
+
+
+
+
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+
+            horizontal = -1.0f;
+        }
+    } 
+
+}
+
+
+
+
