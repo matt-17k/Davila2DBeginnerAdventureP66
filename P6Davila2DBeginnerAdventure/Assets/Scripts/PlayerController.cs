ab6c9e693a36fb4f75a6977c38842a60818383dd
Matthew <769438@student.dusd.net>
2025-12-04 13:47:37 -0800

    added a bunch of stuff type shift
    added movement and some tiles and yeah uhh yes

--- P6Davila2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P6Davila2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+   
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
+        
     {
-
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 5.0f * Time.deltaTime;
         transform.position = position;
-
-
-
-
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-
-            horizontal = -1.0f;
-        }
-    } 
-
+    }
+    
 }
 
 
